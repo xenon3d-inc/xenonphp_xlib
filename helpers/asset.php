5e2d8b38f52d2a420fbec7f83bf332d3e96f3fc0
olivier st-laurent <kzaurckichz@gmail.com>
2018-09-18 01:07:25 -0400

    ...

--- helpers/asset.php
+++ helpers/asset.php
@@ -2,3 +2,21 @@
 function X_asset($path) {
     return BASE_URL.$path.'?'.@filemtime(DOCUMENT_ROOT.BASE_URL.$path);
 }
+function X_asset_js($path, $forceIncludeAlsoInDev = false) {
+    if (PROD || $forceIncludeAlsoInDev) {
+        echo '<script>';
+        include DOCUMENT_ROOT.BASE_URL.$path;
+        echo '</script>';
+    } else {
+        echo '<script src="'.X_asset($path).'"></script>';
+    }
+}
+function X_asset_css($path, $forceIncludeAlsoInDev = false) {
+    if (PROD || $forceIncludeAlsoInDev) {
+        echo '<style>';
+        include DOCUMENT_ROOT.BASE_URL.$path;
+        echo '</style>';
+    } else {
+        echo '<link rel="stylesheet" href="'.X_asset($path).'">';
+    }
+}
