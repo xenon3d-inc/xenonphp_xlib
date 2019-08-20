c18eec0e55bead8df31ce16e673f8c7ce00922c6
olivier st-laurent <kzaurckichz@gmail.com>
2019-08-20 18:03:21 -0400

    support for bool type with null values in conditions

--- Xenon/Db/Query/Helper/Where.php
+++ Xenon/Db/Query/Helper/Where.php
@@ -29,12 +29,16 @@ class Where {
             if (count($args) > 0) {
                 $this->where = array_shift($args);
                 if (!($this->where instanceof Field || $this->where instanceof Expr)) {
+                    $field = null;
+                    $operator = '=';
                     
                     // If match a field name
                     if (preg_match("#^[a-z_]+\w*$#i", $this->where)) {
                         $this->where = new Field($this->model, $this->where);
+                        if ($this->where->modelData) {
+                            $field = $this->where->modelData->getFieldOrColumn($this->where->field);
+                        }
                         if (count($args) > 0) {
-                            $operator = '=';
                             if (count($args) == 2) {
                                 $operator = array_shift($args);
                             } else if (is_array($args[0])) {
@@ -45,18 +49,34 @@ class Where {
                     }
                     
                     if (count($args) > 0) {
-
-                        // Handle special case with empty array IN () / NOT IN ()
-                        if (count($args) == 1 && is_array($args[0]) && empty($args[0])) {
-                            $expression = "".$this->where;
-                            if (preg_match("#^\s*[a-z_]+\w*\s*in\s*\(?\?\)?\s*$#i", $expression)) {
-                                // WHERE field IN ()
-                                $this->where = "0";
-                                return;
-                            } else if (preg_match("#^\s*[a-z_]+\w*\s*not\s*in\s*\(?\?\)?\s*$#i", $expression)) {
-                                // WHERE field NOT IN ()
-                                $this->where = "1";
-                                return;
+                        if (count($args) == 1) {
+                            if ($field && $field->type == 'tinyint' && $operator == '=') {
+                                if ($args[0] === null) {
+                                    $this->where = "$field IS NULL";
+                                    $args = [];
+                                } else if ($args[0] === true) {
+                                    $this->where = "$field IS NOT NULL AND $field = 1";
+                                    $args = [];
+                                } else if ($args[0] === false) {
+                                    $this->where = "$field IS NULL OR $field = 0";
+                                    $args = [];
+                                }
+                            } else {
+                                // Handle special case with empty array IN () / NOT IN ()
+                                if (is_array($args[0]) && empty($args[0])) {
+                                    $expression = "".$this->where;
+                                    if (preg_match("#^\s*[a-z_]+\w*\s*in\s*\(?\?\)?\s*$#i", $expression)) {
+                                        // WHERE field IN ()
+                                        $this->where = "0";
+                                        $args = [];
+                                        return;
+                                    } else if (preg_match("#^\s*[a-z_]+\w*\s*not\s*in\s*\(?\?\)?\s*$#i", $expression)) {
+                                        // WHERE field NOT IN ()
+                                        $this->where = "1";
+                                        $args = [];
+                                        return;
+                                    }
+                                }
                             }
                         }
 
